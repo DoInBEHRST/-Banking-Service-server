14f6d03a49e241a27e33f53fd41a8ac25eaf7926
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-01-22 21:26:30 +0900

    refactor: Member 도메인 lombok 적용
    lombok 적용

--- server/src/main/java/com/bankingservice/server/domain/Member.java
+++ server/src/main/java/com/bankingservice/server/domain/Member.java
@@ -1,7 +1,11 @@
 package com.bankingservice.server.domain;
 
 import java.text.SimpleDateFormat;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Member {
 
     private long mbno;
@@ -13,76 +17,4 @@ public class Member {
     private long pnt;
     private SimpleDateFormat lstMdfMbno;
     private SimpleDateFormat lstMdfDatetime;
-
-    public long getMbno() {
-        return mbno;
-    }
-
-    public void setMbno(long mbno) {
-        this.mbno = mbno;
-    }
-
-    public String getStcd() {
-        return stcd;
-    }
-
-    public void setStcd(String stcd) {
-        this.stcd = stcd;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
-    public String getRegNum() {
-        return regNum;
-    }
-
-    public void setRegNum(String regNum) {
-        this.regNum = regNum;
-    }
-
-    public long getPrtMbno() {
-        return prtMbno;
-    }
-
-    public void setPrtMbno(long prtMbno) {
-        this.prtMbno = prtMbno;
-    }
-
-    public long getPnt() {
-        return pnt;
-    }
-
-    public void setPnt(long pnt) {
-        this.pnt = pnt;
-    }
-
-    public SimpleDateFormat getLstMdfMbno() {
-        return lstMdfMbno;
-    }
-
-    public void setLstMdfMbno(SimpleDateFormat lstMdfMbno) {
-        this.lstMdfMbno = lstMdfMbno;
-    }
-
-    public SimpleDateFormat getLstMdfDatetime() {
-        return lstMdfDatetime;
-    }
-
-    public void setLstMdfDatetime(SimpleDateFormat lstMdfDatetime) {
-        this.lstMdfDatetime = lstMdfDatetime;
-    }
 }
