b91e3ebc9d05861f52862a09b43abe87fa8ea463
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-09 01:02:47 +0900

    feat: MemberService 수정
    Enum 추가에 따른 코드 변경

--- server/src/main/java/com/bankingservice/server/service/MemberService.java
+++ server/src/main/java/com/bankingservice/server/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.bankingservice.server.service;
 
+import static com.bankingservice.server.constants.MemberStcd.STCD_NOT_USE;
+import static com.bankingservice.server.constants.MemberStcd.STCD_USE;
+
 import com.bankingservice.server.domain.Member;
 import com.bankingservice.server.repository.InmemoryMemberRepository;
 import com.bankingservice.server.repository.MemberRepository;
@@ -11,9 +14,6 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    private final String STCD_USE = "01";
-    private final String STCD_NOT_USE = "02";
-
     @Autowired
     public MemberService(InmemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -24,6 +24,11 @@ public class MemberService {
         Member member = memberRepository.findByIdAndPw(id, pw);
         if (member == null) {
             throw new IllegalStateException("아이디 혹은 비밀번호가 다릅니다.");
+        }
+
+        // 사용여부 체크
+        if (member.getStcd().equals(STCD_NOT_USE)) {
+            throw new IllegalStateException("존재하지 않는 아이디 입니다.");
         }
 
         return member;
@@ -43,6 +48,9 @@ public class MemberService {
                 throw new IllegalStateException("잘못된 부모 아이디 입니다.");
             }
         }
+
+        // 사용여부 처리
+        member.setStcd(STCD_USE);
 
         return memberRepository.save(member);
     }
