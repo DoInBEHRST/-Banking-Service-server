983760920f0ef65750b3fd3db6458621ac90e591
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-08 23:49:17 +0900

    feat: MemberLoginForm Builder 어노테이션 추가
    Builder가 여러모로 좋다고 함
    그래서 builder 씀 ㅋㅋㅋ

--- server/src/main/java/com/bankingservice/server/controller/MemberLoginForm.java
+++ server/src/main/java/com/bankingservice/server/controller/MemberLoginForm.java
@@ -1,10 +1,12 @@
 package com.bankingservice.server.controller;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class MemberLoginForm {
 
     private String id;
