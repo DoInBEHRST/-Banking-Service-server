f62aa0e5b8e12669e1329d8856602ebef0341787
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-09 00:04:59 +0900

    test: 회원가입 테스트 추가
    간단하게
    아이디 중복
    부모 아이디 유효성 체크 만 구현해둠

--- server/src/test/java/com/bankingservice/server/service/MemberServiceTest.java
+++ server/src/test/java/com/bankingservice/server/service/MemberServiceTest.java
@@ -1,11 +1,14 @@
 package com.bankingservice.server.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.bankingservice.server.controller.MemberLoginForm;
+import com.bankingservice.server.controller.MemberSignupForm;
+import com.bankingservice.server.domain.Member;
 import com.bankingservice.server.repository.InmemoryMemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 public class MemberServiceTest {
 
     MemberService memberService;
@@ -18,15 +21,80 @@ public class MemberServiceTest {
     }
 
     @Test
-    void 없는_사람_로그인(){
+    void 없는_사람_로그인() {
 
-        MemberLoginForm memberLoginForm = new MemberLoginForm();
-        memberLoginForm.setId("권은비");
-        memberLoginForm.setPw("1234");
+        MemberLoginForm memberLoginForm = MemberLoginForm.builder()
+            .id("권은비")
+            .pw("1234")
+            .build();
 
-        IllegalStateException e = assertThrows(IllegalStateException.class, ()-> memberService.login(memberLoginForm));
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+            () -> memberService.login(memberLoginForm));
 
         assertThat(e.getMessage()).isEqualTo("아이디 혹은 비밀번호가 다릅니다.");
+
+    }
+
+    @Test
+    void 회원가입_테스트() {
+
+        MemberSignupForm newMemberForm = MemberSignupForm.builder()
+            .id("권은비")
+            .pw("1234")
+            .regNum("1234-1234")
+            .isPrt(false)
+            .prtId(null)
+            .build();
+
+        Member newMember = memberService.signup(newMemberForm);
+
+        assertThat(newMember.getId()).isEqualTo("권은비");
+        assertThat(newMember.getPw()).isEqualTo("1234");
+        assertThat(newMember.getRegNum()).isEqualTo("1234-1234");
+
+    }
+
+    @Test
+    void 아이디_중복_테스트() {
+        MemberSignupForm newMemberForm = MemberSignupForm.builder()
+            .id("권은비")
+            .pw("1234")
+            .regNum("1234-1234")
+            .isPrt(false)
+            .prtId(null)
+            .build();
+
+        MemberSignupForm newMemberForm2 = MemberSignupForm.builder()
+            .id("권은비")
+            .pw("1234")
+            .regNum("1234-1234")
+            .isPrt(false)
+            .prtId(null)
+            .build();
+
+        Member newMember = memberService.signup(newMemberForm);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+            () -> memberService.signup(newMemberForm2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 아이디 입니다.");
+
+    }
+
+    @Test
+    void 부모아이디_유효성_테스트() {
+        MemberSignupForm newMemberForm = MemberSignupForm.builder()
+            .id("권은비")
+            .pw("1234")
+            .regNum("1234-1234")
+            .isPrt(true)
+            .prtId("사쿠라")
+            .build();
+
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+            () -> memberService.signup(newMemberForm));
+
+        assertThat(e.getMessage()).isEqualTo("잘못된 부모 아이디 입니다.");
 
     }
 
