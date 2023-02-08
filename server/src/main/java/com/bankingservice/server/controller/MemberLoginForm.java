df05ebcfc3920c7e967b288b424e92c3c5ba32ba
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-08 23:51:47 +0900

    feat: Form에 Builder, Constructor 어노테이션 추가
    빠진 부분 추가

--- server/src/main/java/com/bankingservice/server/controller/MemberLoginForm.java
+++ server/src/main/java/com/bankingservice/server/controller/MemberLoginForm.java
@@ -1,12 +1,16 @@
 package com.bankingservice.server.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class MemberLoginForm {
 
     private String id;
--- server/src/main/java/com/bankingservice/server/controller/MemberSignupForm.java
+++ server/src/main/java/com/bankingservice/server/controller/MemberSignupForm.java
@@ -1,10 +1,16 @@
 package com.bankingservice.server.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class MemberSignupForm {
 
     private String id;
