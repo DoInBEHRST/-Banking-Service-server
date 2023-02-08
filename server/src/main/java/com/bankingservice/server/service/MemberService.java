2aacc5f6da50d451c5751733caa64fa4570e2d75
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-08 23:45:49 +0900

    feat: MemberService singup 구현
    - 아이디 중복체크 기능 구현
    - 부모아이디를 입력했을 경우 부모아이디 유효성 체크

--- server/src/main/java/com/bankingservice/server/service/MemberService.java
+++ server/src/main/java/com/bankingservice/server/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.bankingservice.server.service;
 
 import com.bankingservice.server.controller.MemberLoginForm;
+import com.bankingservice.server.controller.MemberSignupForm;
 import com.bankingservice.server.domain.Member;
 import com.bankingservice.server.repository.InmemoryMemberRepository;
 import com.bankingservice.server.repository.MemberRepository;
@@ -18,13 +19,30 @@ public class MemberService {
     }
 
 
-    public Member login(MemberLoginForm memberLoginForm){
+    public Member login(MemberLoginForm memberLoginForm) {
 
         Member member = memberRepository.findByIdAndPw(memberLoginForm);
-        if(member == null){
+        if (member == null) {
             throw new IllegalStateException("아이디 혹은 비밀번호가 다릅니다.");
         }
 
         return member;
+    }
+
+    public Member signup(MemberSignupForm memberSignupForm) {
+
+        Member idCheckMember = memberRepository.findById(memberSignupForm.getId());
+        if (idCheckMember != null) {
+            throw new IllegalStateException("이미 존재하는 아이디 입니다.");
+        }
+
+        if (memberSignupForm.isPrt()) {
+            Member validParents = memberRepository.findById(memberSignupForm.getPrtId());
+            if (validParents == null) {
+                throw new IllegalStateException("잘못된 부모 아이디 입니다.");
+            }
+        }
+
+        return memberRepository.save(memberSignupForm);
     }
 }
