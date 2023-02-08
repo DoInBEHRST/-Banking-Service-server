44b69665c248964ab5fce97c2367fee5446aa073
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-08 23:42:59 +0900

    feat: Member 도메인 수정
    - Builder 패턴 추가
    - 생성자 추가

--- server/src/main/java/com/bankingservice/server/domain/Member.java
+++ server/src/main/java/com/bankingservice/server/domain/Member.java
@@ -1,11 +1,17 @@
 package com.bankingservice.server.domain;
 
 import java.text.SimpleDateFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Member {
 
     private long mbno;
@@ -17,4 +23,6 @@ public class Member {
     private long pnt;
     private SimpleDateFormat lstMdfMbno;
     private SimpleDateFormat lstMdfDatetime;
+
+
 }
