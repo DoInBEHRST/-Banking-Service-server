4a5d50e66f9f014da51ff1b1a037a919a53b2517
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-03-31 23:48:16 +0900

    fix: Member 엔티티 수정
    - 컬럼 annotaion 추가
    - default 값 설정

--- server/src/main/java/com/bankingservice/server/entity/Member.java
+++ server/src/main/java/com/bankingservice/server/entity/Member.java
@@ -4,27 +4,49 @@ import com.bankingservice.server.constants.MemberStcd;
 import com.bankingservice.server.utill.DateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
+@Getter
+@Setter
+@SuperBuilder
+@DynamicInsert
 @Table(name = "HRST_MEMM_MEM_MNGMT")
 public class Member extends DateTime {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long MBNO;
 
-    @Column(length = 10)
+    @Column(columnDefinition = "varchar(10) default `01`")
     private MemberStcd STCD;
 
     @Column(unique = true, nullable = false)
     private String ID;
 
+    @Column
     private String PW;
+
+    @Column
     private String REG_NUM;
+
+    @Column
     private String NM;
+
+    @Column
     private Long PRT_MBNO;
+
+    @Column(columnDefinition = "INT default 0")
     private Long PNT;
+
+    @Column
     private Long LST_MDF_MBNO;
 
 
