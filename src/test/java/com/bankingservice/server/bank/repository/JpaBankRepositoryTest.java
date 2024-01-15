0e554b4f61d1bad7ab2cfcf17cbf98866baafd39
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2024-01-15 23:45:20 +0900

    test: JpaBankRepositoryTest 수정
    - 테스트 이상하게 하고 있어서 변경함 ㅋㅋ

--- src/test/java/com/bankingservice/server/bank/repository/JpaBankRepositoryTest.java
+++ src/test/java/com/bankingservice/server/bank/repository/JpaBankRepositoryTest.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
+@Transactional
 public class JpaBankRepositoryTest {
 
     @Autowired
@@ -22,15 +24,47 @@ public class JpaBankRepositoryTest {
     @Test
     @DisplayName("사용 가능한 모든 은행코드 조회 테스트")
     void allBankSelectByUsable() {
+
+        Bank kbBank = Bank.builder()
+            .bnkCd(1L)
+            .BNK_NM("국민은행")
+            .STCD(DataStcd.STCD_USE)
+            .build();
+
+        Bank bnkBank = Bank.builder()
+            .bnkCd(2L)
+            .BNK_NM("부산은행")
+            .STCD(DataStcd.STCD_NOT_USE)
+            .build();
+
+        bankRepository.save(kbBank);
+        bankRepository.save(bnkBank);
+
         List<Bank> banks = bankRepository.findBySTCD(DataStcd.STCD_USE);
-        assertThat(banks.size()).isEqualTo(0);
+        assertThat(banks).doesNotContain(bnkBank);
     }
 
     @Test
     @DisplayName("은행 코드로 조회 테스트")
     void selectByBankCode() {
-        Bank bank = bankRepository.findByBnkCdAndSTCD(321L, DataStcd.STCD_USE);
-        assertThat(bank).isNull();
+
+        Bank kbBank = Bank.builder()
+            .bnkCd(1L)
+            .BNK_NM("국민은행")
+            .STCD(DataStcd.STCD_USE)
+            .build();
+
+        Bank bnkBank = Bank.builder()
+            .bnkCd(2L)
+            .BNK_NM("부산은행")
+            .STCD(DataStcd.STCD_NOT_USE)
+            .build();
+
+        bankRepository.save(kbBank);
+        bankRepository.save(bnkBank);
+
+        Bank bank = bankRepository.findByBnkCdAndSTCD(1L, DataStcd.STCD_USE);
+        assertThat(bank.getBNK_NM()).isEqualTo(kbBank.getBNK_NM());
     }
 
 }
